8674714db2ff7da8148013e2c69ba2a53b544ca3
Melanie Shaul <mshaul@usc.edu>
2016-09-12 11:55:10 -0700

    trying background job test 3

--- cloud/jobs/sendDailyNotification.js
+++ cloud/jobs/sendDailyNotification.js
@@ -1,4 +1,8 @@
 
+var Parse = require('parse/node');
+Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY, process.env.MASTER_KEY);
+Parse.serverURL = process.env.SERVER_URL;
+Parse.Cloud.useMasterKey();
 
 
 function sendDailyNotification() {
